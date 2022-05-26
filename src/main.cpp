3a25f41edd0160107925699006cbef13cd79f025
Everton Cavalcante <evertonranielly@gmail.com>
2022-05-26 18:38:37 -0300

    Adicionando base main

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file	main.cpp
+ * @brief	Arquivo fonte principal do programa
+ * @author	<Inserir nome> (<Inserir e-mail>)
+ * @author  <Inserir nome> (<Inserir e-mail>)
+ * @since	26/05/2022
+ * @date    <Incluir data da última atualização do arquivo>
+ */
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main() {
+    cout <<  "DNA Profiler" << endl;
+    cout << "Este programa carrega uma base de dados de DNA e uma sequência desconhecida de DNA" << endl;
+    cout << "e tenta identificar a que indivíduo esse segmento de DNA provavelmente pertence" << endl;
+    cout << endl;
+
+    return 0;
+}
